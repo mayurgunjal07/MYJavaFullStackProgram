b18c8bb8bec853b93605a6cd6464cc62c3ab5ef7
mayurgunjal07 <139338445+mayurgunjal07@users.noreply.github.com>
2023-07-12 02:28:33 -0700

    Add files via upload

---
+++ mayur.java
@@ -0,0 +1,17 @@
+package myfirstprogram;
+
+public class mayur {
+
+	public static void main(String[] args) {
+      System.out.println("Name:Mayur ");
+      System.out.println("Qualification: BSC COMPUTER SCIENCE");
+	}
+	
+}
+
+
+
+	
+
+
+
